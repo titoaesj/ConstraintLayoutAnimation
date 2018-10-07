a8a99137e5bec2ddf5c5d656ed16040df31eeecb
Tito Albino Evangelista da Silva Junior <titoaesj@gmail.com>
2018-10-06 22:52:49 -0300

    Melhorias no layout

--- app/src/main/java/titoaesj/com/br/constraintlayoutanimation/MainActivity.kt
+++ app/src/main/java/titoaesj/com/br/constraintlayoutanimation/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.annotation.RequiresApi
 import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
 import android.transition.TransitionManager
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -27,9 +28,29 @@ class MainActivity : AppCompatActivity() {
             TransitionManager.beginDelayedTransition(imageRoot)
             if (setOf) {
                 mConstraintSet2.applyTo(imageRoot)
+                circle_left.visibility = View.VISIBLE
+                circle_rigth.visibility = View.VISIBLE
+                dotted_line.visibility = View.VISIBLE
+                qrcode.visibility = View.VISIBLE
+                terminal_label.visibility = View.VISIBLE
+                terminal_value.visibility = View.VISIBLE
+                gate_label.visibility = View.VISIBLE
+                gate_value.visibility = View.VISIBLE
+                boarding_label.visibility = View.VISIBLE
+                boarding_value.visibility = View.VISIBLE
                 setOf = false
             } else {
                 mConstraintSet1.applyTo(imageRoot)
+                circle_left.visibility = View.GONE
+                circle_rigth.visibility = View.GONE
+                dotted_line.visibility = View.GONE
+                qrcode.visibility = View.GONE
+                terminal_label.visibility = View.GONE
+                terminal_value.visibility = View.GONE
+                gate_label.visibility = View.GONE
+                gate_value.visibility = View.GONE
+                boarding_label.visibility = View.GONE
+                boarding_value.visibility = View.GONE
                 setOf = true
             }
         }
